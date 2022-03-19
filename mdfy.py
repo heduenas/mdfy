863e0d5e777cf7f21cb7e9a0254b0b7130d7eb21
Hugo Dueñas <heduenas@users.noreply.github.com>
2022-03-18 20:59:00 -0700

    Create mdfy.py

---
+++ mdfy.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+import sys
+import re
+
+def convertTable(match):
+    latex = match.group()
+    latex = re.sub(r"\\begin{tabular}(?:\{([^\}]*)\})?", r"", latex)
+    latex = re.sub(r"\\end\{tabular\}", r"", latex)
+    latex = latex.strip()
+    latex = re.sub(r"\\hline$", r"", latex)
+
+    latex = re.sub(r"\\hline\n", r"| ", latex)
+    latex = re.sub(r"\\\\\n", r" |\n", latex)
+    latex = re.sub(r"&", r" | ", latex)
+
+    numberOfColumns = latex[0:latex.find("\n")].count("|") - 1
+    latex = "| " * numberOfColumns + "|\n" + "|---" * numberOfColumns + "|\n" + latex
+    
+    return latex
+
+def mdfy(latex):
+    latex = re.sub(r"(\\section{Exercises})((.|\n)*)", r"# Exercises\n\nTODO: Add exercises from C++ course", latex, flags=re.MULTILINE)
+
+    # Text formating
+    latex = re.sub(r"\{\\em ([^}]*)\}", r"*\g<1>*", latex)
+    latex = re.sub(r"\{\\it ([^}]*)\}", r"*\g<1>*", latex)
+    latex = re.sub(r"\{\\bf ([^}]*)\}", r"**\g<1>**", latex)
+    latex = re.sub(r"``([^']*)''", r'"\g<1>"', latex)
+    latex = re.sub(r"\\url{([^}]*)}", r"[\g<1>](\g<1>)", latex)
+
+    latex = re.sub(r"\\\{", "{", latex)
+    latex = re.sub(r"\\\}", "}", latex)
+    latex = re.sub(r"\\%", "%", latex)
+    latex = re.sub(r"\\ldots", "...", latex)
+
+    latex = re.sub(r"\\begin\{small\}", "", latex)
+    latex = re.sub(r"\\end\{small\}", "", latex)
+
+    # Sections and subsections
+    latex = re.sub(r"\\chapter\{([^}]*)\}", r"# \g<1>", latex)
+    latex = re.sub(r"\\section\{([^}]*)\}", r"## \g<1>", latex)
+
+    # Codeblocks
+    latex = re.sub(r"\\java\{([^}]*)\}", r"`\g<1>`", latex)
+    latex = re.sub(r"\{\\tt ([^}]*)\}", r"`\g<1>`", latex)
+    latex = re.sub(r"\\begin\{code\}", r"```", latex)
+    latex = re.sub(r"\\end\{code\}", r"```", latex)
+    latex = re.sub(r"\\begin\{stdout\}", r"```", latex)
+    latex = re.sub(r"\\end\{stdout\}", r"```", latex)
+    latex = re.sub(r"\\begin\{trinket\}(\[[0-9]+\])?\{([^}]*)\}", r"```", latex)
+    latex = re.sub(r"\\end\{trinket\}", r"```", latex)
+    latex = re.sub(r'\\verb"([^"]*)"', r'`\g<1>`', latex)
+    latex = re.sub(r"\\verb'([^']*)'", r'`\g<1>`', latex)
+
+    # Comments and unnecesary tags
+    latex = re.sub(r"^\%([^\n]*)\n", r"", latex, flags=re.MULTILINE)
+    latex = re.sub(r"\\index\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\label\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\begin\{itemize\}", r"", latex)
+    latex = re.sub(r"\\end\{itemize\}", r"", latex)
+    latex = re.sub(r"\\begin\{enumerate\}", r"", latex)
+    latex = re.sub(r"\\end\{enumerate\}", r"", latex)
+
+    # Spacing
+    latex = re.sub(r"\\vspace\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\hspace\{([^}]*)\}", r"", latex)
+
+    # Items
+    latex = re.sub(r"\n\n\\item", r"\n  *", latex)
+    latex = re.sub(r"\\item\[([^\]]*)\]", r"  * **\g<1>**", latex)
+    latex = re.sub(r"\\item ([^\n]*)", r"  * \g<1>", latex)
+
+    # Equations
+    latex = re.sub(r"\\\[([^\]]*)\\\]", r"$\g<1>$", latex)
+
+    #images
+    latex = re.sub(r"\\includegraphics\{([^}]*)\}", r"![](\g<1>)", latex)
+    latex = re.sub(r"\\caption\{([^}]*)\}", r"<p>\g<1></p>", latex)
+    latex = re.sub(r"\\begin\{figure\}(\[([^\]]*)\])?", r"", latex)
+    latex = re.sub(r"\\begin\{center\}", r"", latex)
+    latex = re.sub(r"\\end\{figure\}", r"", latex)
+    latex = re.sub(r"\\end\{center\}", r"", latex)
+    latex = re.sub(r"\\includegraphics(\[([^\]]*)\])?", r"![](\g<1>)", latex)
+
+    # Vocabullary definition
+    latex = re.sub(r"\\begin\{description\}", r"", latex)
+    latex = re.sub(r"\\end\{description\}", r"", latex)
+    latex = re.sub(r"\\term\{([^}]*)\}", r"***\g<1>:***", latex)
+    latex = re.sub(r"\\term\{([^}]*)\}", r"***\g<1>:***", latex)
+
+    # Table
+    latex = re.sub(r"\\begin\{table\}(\[[^\]]+\])?", r"", latex)
+    latex = re.sub(r"(?:\\begin{tabular})(?:\{([^\}]*)\})?((.|\n)*)(?:\\end{tabular})", convertTable, latex, flags=re.MULTILINE)
+    latex = re.sub(r"\\end\{table\}", r"", latex)
+
+    # Quote
+    latex = re.sub(r"(?:\\begin{quote}\n)(?:\{([^\}]*)\})?((.|\n)*)(?:\\end{quote})", r"> \g<2>", latex, flags=re.MULTILINE)
+
+    return latex
+
+def main(argv):
+    inputfile = argv[0]
+    outputfile = argv[1]
+    
+
+    with open(inputfile, 'r') as texFile:
+        latex = texFile.read().rstrip()
+
+    markdown = mdfy(latex)
+
+    mdFile = open(outputfile, "w")
+    mdFile.write(markdown)
+    mdFile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
