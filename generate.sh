7c14f58657537bf146f2d80c0bd2eb767815b9c6
Hugo Dueñas <heduenas@users.noreply.github.com>
2022-03-20 16:59:44 -0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -16,14 +16,23 @@ cp -r figs/* 6/statements
 cp -r figs/* 7/statements
 cp -r figs/* 8/statements
 
-python3 mdfy.py ch01.tex 1/statements/en.md
-python3 mdfy.py ch02.tex 2/statements/en.md
-python3 mdfy.py ch03.tex 3/statements/en.md
-python3 mdfy.py ch04.tex 4/statements/en.md
-python3 mdfy.py ch05.tex 5/statements/en.md
-python3 mdfy.py ch06.tex 6/statements/en.md
-python3 mdfy.py ch07.tex 7/statements/en.md
-python3 mdfy.py ch08.tex 8/statements/en.md
+python3 mdfy.py ch01.tex 1/statements/en.markdown
+python3 mdfy.py ch02.tex 2/statements/en.markdown
+python3 mdfy.py ch03.tex 3/statements/en.markdown
+python3 mdfy.py ch04.tex 4/statements/en.markdown
+python3 mdfy.py ch05.tex 5/statements/en.markdown
+python3 mdfy.py ch06.tex 6/statements/en.markdown
+python3 mdfy.py ch07.tex 7/statements/en.markdown
+python3 mdfy.py ch08.tex 8/statements/en.markdown
+
+cp 1/statements/en.markdown 1/statements/es.markdown
+cp 2/statements/en.markdown 2/statements/es.markdown
+cp 3/statements/en.markdown 3/statements/es.markdown
+cp 4/statements/en.markdown 4/statements/es.markdown
+cp 5/statements/en.markdown 5/statements/es.markdown
+cp 6/statements/en.markdown 6/statements/es.markdown
+cp 7/statements/en.markdown 7/statements/es.markdown
+cp 8/statements/en.markdown 8/statements/es.markdown
 
 zip -r 1.zip 1/
 zip -r 2.zip 2/
@@ -34,3 +43,11 @@ zip -r 6.zip 6/
 zip -r 7.zip 7/
 zip -r 8.zip 8/
 
+rm -r 1/
+rm -r 2/
+rm -r 3/
+rm -r 4/
+rm -r 5/
+rm -r 6/
+rm -r 7/
+rm -r 8/
