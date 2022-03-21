89ee77cd0a08045d989901f8ffb9da8881a571d5
Hugo Dueñas <heduenas@users.noreply.github.com>
2022-03-20 17:15:58 -0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -7,15 +7,6 @@ mkdir 6; mkdir 6/statements
 mkdir 7; mkdir 7/statements
 mkdir 8; mkdir 8/statements
 
-cp -r figs/* 1/statements
-cp -r figs/* 2/statements
-cp -r figs/* 3/statements
-cp -r figs/* 4/statements
-cp -r figs/* 5/statements
-cp -r figs/* 6/statements
-cp -r figs/* 7/statements
-cp -r figs/* 8/statements
-
 python3 mdfy.py ch01.tex 1/statements/en.markdown
 python3 mdfy.py ch02.tex 2/statements/en.markdown
 python3 mdfy.py ch03.tex 3/statements/en.markdown
@@ -33,6 +24,20 @@ cp 5/statements/en.markdown 5/statements/es.markdown
 cp 6/statements/en.markdown 6/statements/es.markdown
 cp 7/statements/en.markdown 7/statements/es.markdown
 cp 8/statements/en.markdown 8/statements/es.markdown
+
+sudo apt install dia
+cd figs
+for D in *.dia; do dia -t png -s 600x $D; done
+cd ..
+
+cp -r figs/* 1/statements
+cp -r figs/* 2/statements
+cp -r figs/* 3/statements
+cp -r figs/* 4/statements
+cp -r figs/* 5/statements
+cp -r figs/* 6/statements
+cp -r figs/* 7/statements
+cp -r figs/* 8/statements
 
 zip -r 1.zip 1/
 zip -r 2.zip 2/
