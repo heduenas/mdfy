7e4a78784ebc276a7c4c71a245bc9edb7c506ffc
Hugo Dueñas <heduenas@users.noreply.github.com>
2022-03-26 15:50:24 -0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,58 +1,151 @@
-mkdir 1; mkdir 1/statements
-mkdir 2; mkdir 2/statements
-mkdir 3; mkdir 3/statements
-mkdir 4; mkdir 4/statements
-mkdir 5; mkdir 5/statements
-mkdir 6; mkdir 6/statements
-mkdir 7; mkdir 7/statements
-mkdir 8; mkdir 8/statements
+#!/usr/bin/python
+import sys
+import re
 
-python3 mdfy.py ch01.tex 1/statements/en.markdown
-python3 mdfy.py ch02.tex 2/statements/en.markdown
-python3 mdfy.py ch03.tex 3/statements/en.markdown
-python3 mdfy.py ch04.tex 4/statements/en.markdown
-python3 mdfy.py ch05.tex 5/statements/en.markdown
-python3 mdfy.py ch06.tex 6/statements/en.markdown
-python3 mdfy.py ch07.tex 7/statements/en.markdown
-python3 mdfy.py ch08.tex 8/statements/en.markdown
+def convertQuotes(latex):
+    startTag = "\\begin{quote}"
+    endTag = "\\end{quote}"
 
-cp 1/statements/en.markdown 1/statements/es.markdown
-cp 2/statements/en.markdown 2/statements/es.markdown
-cp 3/statements/en.markdown 3/statements/es.markdown
-cp 4/statements/en.markdown 4/statements/es.markdown
-cp 5/statements/en.markdown 5/statements/es.markdown
-cp 6/statements/en.markdown 6/statements/es.markdown
-cp 7/statements/en.markdown 7/statements/es.markdown
-cp 8/statements/en.markdown 8/statements/es.markdown
+    while True:
+        endPos = latex.find(endTag)
+        if endPos == -1:
+            break
+        endPos += len(endTag)
+        startPos = latex.rfind(startTag, 0, endPos)
 
-sudo apt install dia
-cd figs
-for D in *.dia; do dia -t png -s 600x $D; done
-cd ..
+        substring = latex[startPos:endPos]
+        mdQuoted = substring
+        mdQuoted = re.sub(r"\n", r" \n >", mdQuoted, flags=re.MULTILINE)
+        mdQuoted = mdQuoted.replace(startTag, "")
+        mdQuoted = mdQuoted.replace(endTag, "")
+        latex = latex.replace(substring, mdQuoted)
 
-cp -r figs/* 1/statements
-cp -r figs/* 2/statements
-cp -r figs/* 3/statements
-cp -r figs/* 4/statements
-cp -r figs/* 5/statements
-cp -r figs/* 6/statements
-cp -r figs/* 7/statements
-cp -r figs/* 8/statements
+    return latex
 
-zip -r 1.zip 1/
-zip -r 2.zip 2/
-zip -r 3.zip 3/
-zip -r 4.zip 4/
-zip -r 5.zip 5/
-zip -r 6.zip 6/
-zip -r 7.zip 7/
-zip -r 8.zip 8/
+def convertTable(match):
+    latex = match.group()
+    latex = re.sub(r"\\begin{tabular}(?:\{([^\}]*)\})?", r"", latex)
+    latex = re.sub(r"\\end\{tabular\}", r"", latex)
+    latex = latex.strip()
+    latex = re.sub(r"\\hline$", r"", latex)
 
-rm -r 1/
-rm -r 2/
-rm -r 3/
-rm -r 4/
-rm -r 5/
-rm -r 6/
-rm -r 7/
-rm -r 8/
+    latex = re.sub(r"\\hline\n", r"| ", latex)
+    latex = re.sub(r"\\\\\n", r" |\n", latex)
+    latex = re.sub(r"&", r" | ", latex)
+
+    numberOfColumns = latex[0:latex.find("\n")].count("|") - 1
+    latex = "| " * numberOfColumns + "|\n" + "|---" * numberOfColumns + "|\n" + latex
+    
+    return latex
+
+def convertEquationArray(match):
+    latex = match.group()
+    latex = re.sub(r"\\\\", r" \\newline", latex)
+    
+    return latex
+
+def mdfy(latex):
+    latex = re.sub(r"(\\section{Exercises})((.|\n)*)", r"", latex, flags=re.MULTILINE)
+
+    # Text formating
+    latex = re.sub(r"\{\\em ([^}]*)\}", r"*\g<1>*", latex)
+    latex = re.sub(r"\{\\it ([^}]*)\}", r"*\g<1>*", latex)
+    latex = re.sub(r"\{\\bf ([^}]*)\}", r"**\g<1>**", latex)
+    latex = re.sub(r"``([^']*)''", r'"\g<1>"', latex)
+    latex = re.sub(r"\\url{([^}]*)}", r"[\g<1>](\g<1>)", latex)
+
+    latex = re.sub(r"\\\{", "{", latex)
+    latex = re.sub(r"\\\}", "}", latex)
+    latex = re.sub(r"\\%", "%", latex)
+    latex = re.sub(r"\\ldots", "...", latex)
+
+    latex = re.sub(r"\\begin\{small\}", "", latex)
+    latex = re.sub(r"\\end\{small\}", "", latex)
+
+    latex = re.sub(r"\{\\sf ([^}]*)\}", r"**\g<1>**", latex)
+    latex = re.sub(r"\\textbf\{([^}]*)\}", r"**\g<1>**", latex)
+
+    # Sections and subsections
+    latex = re.sub(r"\\chapter\{([^}]*)\}", r"# \g<1>", latex)
+    latex = re.sub(r"\\section\{([^}]*)\}", r"## \g<1>", latex)
+
+    # Codeblocks
+    latex = re.sub(r"\\java\{([^}]*)\}", r"`\g<1>`", latex)
+    latex = re.sub(r"\{\\tt ([^}]*)\}", r"`\g<1>`", latex)
+    latex = re.sub(r"\\begin\{code\}", r"```", latex)
+    latex = re.sub(r"\\end\{code\}", r"```", latex)
+    latex = re.sub(r"\\begin\{stdout\}", r"```", latex)
+    latex = re.sub(r"\\end\{stdout\}", r"```", latex)
+    latex = re.sub(r"\\begin\{trinket\}(\[[0-9]+\])?\{([^}]*)\}", r"```", latex)
+    latex = re.sub(r"\\end\{trinket\}", r"```", latex)
+    latex = re.sub(r'\\verb"([^"]*)"', r'`\g<1>`', latex)
+    latex = re.sub(r"\\verb'([^']*)'", r'`\g<1>`', latex)
+
+    # Comments and unnecesary tags
+    latex = re.sub(r"^\%([^\n]*)\n", r"", latex, flags=re.MULTILINE)
+    latex = re.sub(r"\\index\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\label\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\begin\{itemize\}", r"", latex)
+    latex = re.sub(r"\\end\{itemize\}", r"", latex)
+    latex = re.sub(r"\\begin\{enumerate\}", r"", latex)
+    latex = re.sub(r"\\end\{enumerate\}", r"", latex)
+
+    # Spacing
+    latex = re.sub(r"\\vspace\{([^}]*)\}", r"", latex)
+    latex = re.sub(r"\\hspace\{([^}]*)\}", r"", latex)
+
+    # Items
+    latex = re.sub(r"\n\n\\item", r"\n  *", latex)
+    latex = re.sub(r"\\item\[([^\]]*)\]", r"  * **\g<1>**", latex)
+    latex = re.sub(r"\\item ([^\n]*)", r"  * \g<1>", latex)
+
+    # Equations
+    latex = re.sub(r"\\\[([^\]]*)\\\]", r"$\g<1>$", latex)
+
+    #images
+    latex = re.sub(r"figs/([^\.]*)\.pdf", r"\g<1>.png", latex)
+    latex = re.sub(r"figs/([^\.]*)\.jpg", r"\g<1>.jpg", latex)
+    latex = re.sub(r"\\includegraphics\{([^}]*)\}", r"![\g<1>](\g<1>)", latex)
+    latex = re.sub(r"\\caption\{([^}]*)\}", r"<p>\g<1></p>", latex)
+    latex = re.sub(r"\\begin\{figure\}(\[([^\]]*)\])?", r"", latex)
+    latex = re.sub(r"\\begin\{center\}", r"", latex)
+    latex = re.sub(r"\\end\{figure\}", r"", latex)
+    latex = re.sub(r"\\end\{center\}", r"", latex)
+    latex = re.sub(r"\\includegraphics(?:\[([^\]]*)\])?\{([^}]*)\}", r"![\g<2>](\g<2>)", latex)
+
+    # Vocabullary definition
+    latex = re.sub(r"\\begin\{description\}", r"", latex)
+    latex = re.sub(r"\\end\{description\}", r"", latex)
+    latex = re.sub(r"\\term\{([^}]*)\}", r"***\g<1>:***", latex)
+    latex = re.sub(r"\\term\{([^}]*)\}", r"***\g<1>:***", latex)
+
+    # Table
+    latex = re.sub(r"\\begin\{table\}(\[[^\]]+\])?", r"", latex)
+    latex = re.sub(r"(?:\\begin{tabular})(?:\{([^\}]*)\})?((.|\n)*)(?:\\end{tabular})", convertTable, latex, flags=re.MULTILINE)
+    latex = re.sub(r"\\end\{table\}", r"", latex)
+    
+    # Equation array
+    latex = re.sub(r"(?:\\begin{eqnarray\*})((.|\n)*)(?:\\end{eqnarray\*})", convertEquationArray, latex, flags=re.MULTILINE)
+
+    # Quotes
+    latex = convertQuotes(latex)
+
+    return latex
+
+def main(argv):
+    inputfile = argv[0]
+    outputfile = argv[1]
+    
+
+    with open(inputfile, 'r') as texFile:
+        latex = texFile.read().rstrip()
+
+    markdown = mdfy(latex)
+
+    mdFile = open(outputfile, "w")
+    mdFile.write(markdown)
+    mdFile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
